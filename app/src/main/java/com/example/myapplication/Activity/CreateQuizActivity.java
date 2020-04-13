634475e991b40d785d10a2056e645a3006dbb858
Vismark Juarez <vismarkjuarez@gmailcom>
2020-04-13 00:16:42 -0500

    Quiz creation functionality in place.

--- app/src/main/java/com/example/myapplication/Activity/CreateQuizActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/CreateQuizActivity.java
@@ -5,19 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
+import com.example.myapplication.Models.MultipleChoiceQuestion;
+import com.example.myapplication.Models.MultipleChoiceQuestion1;
 import com.example.myapplication.R;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateQuizActivity extends AppCompatActivity {
 
     //Instantiating, but not initializing, member variables
-    private String userName, quizName;
+    private static String userName, quizName;
 
     //UI Component references
     private EditText quizNameEditText;
     private Button createQuizButton;
+
+    ListView listview;
+    Button addNewQuestionButton;
+
+    private static List<MultipleChoiceQuestion1> listOfQuizQuestions
+            = new ArrayList<MultipleChoiceQuestion1>();
+
+    private static List<String> listOfQuestionPrompts
+            = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +43,40 @@ public class CreateQuizActivity extends AppCompatActivity {
 
         //Initialize all variables
         initializeMemberVariables();
+
+        findViewsByIds();
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>
+                (CreateQuizActivity.this, android.R.layout.simple_list_item_1, listOfQuestionPrompts);
+
+        MultipleChoiceQuestion1 newMultipleChoiceQuestion1;
+
+        if(getIntent().getStringExtra("EXTRA_USER_NAME") != null) {
+            //The userName is passed in from the previous activity as "EXTRA_USER_NAME"
+            userName = getIntent().getStringExtra("EXTRA_USER_NAME");
+            System.out.println("Username set to: " + userName);
+        }
+
+        if(getIntent().getStringExtra("newQuestion") != null) {
+            Gson gson = new Gson();
+            newMultipleChoiceQuestion1 = gson.fromJson(getIntent().getStringExtra("newQuestion"),
+                    MultipleChoiceQuestion1.class);
+
+            listOfQuizQuestions.add(newMultipleChoiceQuestion1);
+            System.out.println("List of questions is of size: " + listOfQuizQuestions.size());
+
+            //store the new prompt to be displayed in the ListView
+            listOfQuestionPrompts.add(newMultipleChoiceQuestion1.getPrompt());
+
+            //store the entire multipleChoiceQuestion to be used when the quiz is activated.
+            listOfQuizQuestions.add(newMultipleChoiceQuestion1);
+
+            //notify the ListView that a new item (question prompt) has been added to the list
+            arrayAdapter.notifyDataSetChanged();
+        }
+
+        //Linking the arrayAdapter to the ListView
+        listview.setAdapter(arrayAdapter);
 
         //set up all onClick listeners
         setupOnClickListeners();
@@ -36,9 +87,6 @@ public class CreateQuizActivity extends AppCompatActivity {
      * including all relevant UI components.
      */
     private void initializeMemberVariables() {
-        //The userName is passed in from the previous activity as "EXTRA_USER_NAME"
-        userName = getIntent().getStringExtra("EXTRA_USER_NAME");
-
         //initializing the references to all relevant UI components
         quizNameEditText = (EditText)findViewById(R.id.createQuizActivity_quizNameEditText);
         createQuizButton = (Button)findViewById(R.id.createQuizActivity_createQuizButton);
@@ -62,17 +110,30 @@ public class CreateQuizActivity extends AppCompatActivity {
                 /**
                  * Set the `nextActivity` to be the `CreateQuizActivity`
                  */
+                //TODO: Define logic for when the user has finished creating all of the quiz's questions and is ready to start the session.
+
                 Intent nextActivity = new Intent(CreateQuizActivity.this,
-                        CreateQuestionActivity.class);
+                        MainActivity.class);
 
                 //Passing in "extra" data to the nextActivity
                 nextActivity.putExtra("EXTRA_USER_NAME", userName);
                 nextActivity.putExtra("EXTRA_QUIZ_NAME", quizName);
+                //TODO pass the listOfQuizQuestions to the MainActivity
 
                 //start the nextActivity
                 startActivity(nextActivity);
             }
         });
+
+
+        addNewQuestionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CreateQuestionActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     /**
@@ -84,4 +145,30 @@ public class CreateQuizActivity extends AppCompatActivity {
     private String extractQuizName() {
         return quizNameEditText.getText().toString();
     }
+
+    private void findViewsByIds() {
+        listview = findViewById(R.id.listView);
+        addNewQuestionButton = findViewById(R.id.addNewQuestionButton);
+    }
+
+
+    private void checkForNewReceivedQuestion() {
+        MultipleChoiceQuestion1 newMultipleChoiceQuestion1;
+
+        if(getIntent().getStringExtra("newQuestion") != null) {
+            Gson gson = new Gson();
+            newMultipleChoiceQuestion1 = gson.fromJson(getIntent().getStringExtra("newQuestion"),
+                    MultipleChoiceQuestion1.class);
+
+            listOfQuizQuestions.add(newMultipleChoiceQuestion1);
+            System.out.println("List of questions is of size: " + listOfQuizQuestions.size());
+
+            //store the new prompt to be displayed in the ListView
+            listOfQuestionPrompts.add(newMultipleChoiceQuestion1.getPrompt());
+
+            //store the entire multipleChoiceQuestion to be used when the quiz is activated.
+            listOfQuizQuestions.add(newMultipleChoiceQuestion1);
+        }
+    }
+
 }
